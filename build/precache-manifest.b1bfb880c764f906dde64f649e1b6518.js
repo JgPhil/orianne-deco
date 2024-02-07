b0cd94c323c94e9201b70dddcf55637f7b0018b7
JgPhil <jamingph@gmail.com>
2024-02-07 21:36:39 +0100

    new video

---
+++ build/precache-manifest.b1bfb880c764f906dde64f649e1b6518.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05a1ecc303277fe7b07444a060e10c6e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "16310fd4ba8b5345610f",
+    "url": "/static/css/2.186443da.chunk.css"
+  },
+  {
+    "revision": "9638f09a802d5bdd2ef3",
+    "url": "/static/css/main.58a110be.chunk.css"
+  },
+  {
+    "revision": "16310fd4ba8b5345610f",
+    "url": "/static/js/2.2eb264db.chunk.js"
+  },
+  {
+    "revision": "67525790bb67eb85a77468acb0f00f95",
+    "url": "/static/js/2.2eb264db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9638f09a802d5bdd2ef3",
+    "url": "/static/js/main.797a2910.chunk.js"
+  },
+  {
+    "revision": "97c002b8f8a25334b102",
+    "url": "/static/js/runtime-main.67d84520.js"
+  },
+  {
+    "revision": "ab6534b46bd2fd30d201f6fe8d631a91",
+    "url": "/static/media/fond1.ab6534b4.jpg"
+  }
+]);
